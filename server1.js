bf996f79ac57750b9ecc4efe878babf0305d8ff6
BennyNoyman <bn0504127633@gmail.com>
2023-08-16 11:06:27 +0300

    stage4

--- server1.js
+++ server1.js
@@ -2,8 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const {v4: gen} = require('uuid');
+const bcrypt = require('bcrypt');
 const app = express();
 const port = 3000;
+async function cryptMw(req, res, next) {
+    req.body.password = bcrypt.hashSync(req.body.password, 5)
+    next();
+}
 const users =
     [{id: gen(), email: 'ewef@gmail.com', password: 're45fea'},
     {id: gen(), email: 'lk@gmail.com', password: 're45090a'},
@@ -19,12 +24,12 @@ app.get('/:id', (req, res) => {
 app.get('/', (req, res) => {
     res.status(200).json(users);
 })
-app.post("/", (req, res) => {
+app.post("/", cryptMw, (req, res) => {
     req.body.id = gen();
     users.push(req.body);
     res.status(200).json(users);
 })
-app.put('/:id', (req, res) => {
+app.put('/:id', cryptMw, (req, res) => {
     const user = users.find(user => user.id === req.params.id);
     if (req.body.email) user.email = req.body.email;
     if (req.body.password) user.password = req.body.password;
@@ -35,10 +40,14 @@ app.delete('/:id', (req, res) => {
     res.status(200).json(users);
 })
 app.post("/login", (req, res) => {
+    let found = false;
     users.forEach(user => {
-        if (req.body.email === user.email && req.body.password === user.password) res.status(200).send(`User is connected`);
-        res.status(400).send(`wrong credentials`);
+        if (req.body.email === user.email && bcrypt.compareSync(req.body.password, user.password)) {
+            found = true;
+            return res.status(200).send(`User is connected`);
+        }
     })
+    if (!found) res.status(400).send(`wrong credentials`);
 })
 app.listen(port, () => {
     console.log(`Server is up and running on port:${port}`);
