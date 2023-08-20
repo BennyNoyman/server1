20e7f4cde30e9dce873ed7863850b6509ee2a62c
BennyNoyman <bn0504127633@gmail.com>
2023-08-20 16:47:58 +0300

    stage8, finished for now

--- server1.js
+++ server1.js
@@ -24,7 +24,7 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/:id', async (req, res) => {
+app.get('/user/:id', async (req, res) => {
     const users = await read('./users.json');
     const user = users.find(user => user.id === req.params.id);
     if (user) {
@@ -33,19 +33,18 @@ app.get('/:id', async (req, res) => {
         res.status(400).send('user not found');
     }
 })
-app.get('/', async(req, res) => {
+app.get('/users', async(req, res) => {
     const users = await read('./users.json');
     res.status(200).json(users);
 })
-app.post("/", validate, cryptMw, async (req, res) => {
+app.post("/user", validate, cryptMw, async (req, res) => {
     const users = await read('./users.json');
-    console.log(users);
     req.body.id = gen();
     users.push(req.body);
     await write('./users.json', users);
     res.status(200).json(users);
 })
-app.put('/:id',validate, cryptMw, async (req, res) => {
+app.put('/email&password/:id',validate, cryptMw, async (req, res) => {
     const users = await read('./users.json');
     const user = users.find(user => user.id === req.params.id);
     if (req.body.email) {
@@ -57,7 +56,7 @@ app.put('/:id',validate, cryptMw, async (req, res) => {
     await write('./users.json', users);
     res.status(200).json(user);
 })
-app.delete('/:id', async (req, res) => {
+app.delete('/user/:id', async (req, res) => {
     const users = await read('./users.json');
     const userIndex = users.findIndex(user => req.params.id === user.id);
     users.splice(userIndex, 1);
@@ -71,6 +70,18 @@ app.post("/login", async (req, res) => {
     }else {
         res.status(400).send(`wrong credentials`);
     }
+})
+app.put('/product/:id', async (req, res) => {
+    const users = await read('./users.json');
+    const randProduct = Math.round(Math.random() * 101)
+    const user = users.find(user => user.id === req.params.id);
+    const productResponse = await fetch('https://dummyjson.com/products/' + randProduct);
+    const updatedUser = { ...user};
+    updatedUser.product = await productResponse.json();
+    users.push(updatedUser);
+    await write('./users.json', users);
+    console.log(await fetch('https://jsonplaceholder.typicode.com/users', {method: 'post', body: JSON.stringify(updatedUser)}));
+    res.status(200).send(updatedUser);
 })
 app.listen(port, () => {
     console.log(`Server is up and running on port:${port}`);
